f60780a28d18959750a69c665256bd57e0baddb0
mahdi <mahdi.gareno@gmail.com>
2026-09-05 22:49:56 +0330

    chore(repo): update README for clarity and add additional gitignore entries
    Enhance documentation in the README for better understanding and include new entries in the gitignore to improve repository hygiene.

---
+++ android/app/src/main/java/com/example/supplementauthenticitytracker/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.supplementauthenticitytracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.supplementauthenticitytracker.ui.theme.SupplementAuthenticityTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SupplementAuthenticityTrackerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SupplementAuthenticityTrackerTheme {
+        Greeting("Android")
+    }
+}
